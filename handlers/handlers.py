f96cb64d53d6807a757c08f98639557336335176
nikitosabrikos <nikitosabrikos985@gmail.com>
2026-01-20 18:45:37 +0300

    m

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,8 +1,19 @@
-from aiogram.types import Message
+from aiogram.types import Message, BotCommand
 from aiogram import Router
 
 
 basic_router = Router()
+
+
+@basic_router.message(BotCommand(command="start"))
+async def start_comm(message: Message):
+    from main import app
+
+    storage = app.state.redis_client
+    content = message.text
+    storage.set("user_msg", content)
+    value_str = storage.get("user_msg").decode("utf-8")
+    await message.answer(text=f"Saved value:{value_str}!")
 
 
 @basic_router.message()
